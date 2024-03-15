04673d86fb1909ecbccb6564af0b63e7cf680049
M Naufa Dzulfiqar <muhammadnaufa007@gmail.com>
2024-03-15 16:25:47 +0700

    configuration in model and controller for barang

--- app/Http/Controllers/BarangController.php
+++ app/Http/Controllers/BarangController.php
@@ -3,8 +3,41 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Barang;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Auth;
 
 class BarangController extends Controller
 {
-    //
+    public function index(Request $request)
+    {
+        $data = Barang::all();
+        return view('barang.index', compact('data'));
+    }
+    public function create()
+    {
+        return view('barang.create');
+    }
+    public function store(Request $request)
+    {
+        $input = $request->all();
+        $barang = Barang::create($input);
+        return redirect('barang.index')->with('success', 'Data Berhasil Disimpan');
+    }
+    public function edit(Barang $barang)
+    {
+        return view('barang.create');
+    }
+    public function update(Barang $barang, Request $request)
+    {
+        $input = $request->all();
+        $barang->update($input);
+        return redirect(to: 'barang.index')->with('success', 'Data Berhasil Diupdate');
+    }
+    public function destroy($id)
+    {
+        $data = Barang::find($id);
+        $data->delete();
+        return redirect('barang.index')->with('success', 'Data Berhasil Dihapus');
+    }
 }
--- app/Models/Barang.php
+++ app/Models/Barang.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Barang extends Model
 {
     use HasFactory;
+    protected $table = 'barang';
+    protected $guarded = ['id'];
 }
