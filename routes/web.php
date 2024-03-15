3c8fc9173b628da82030255095357c27f67dfc97
M Naufa Dzulfiqar <muhammadnaufa007@gmail.com>
2024-03-15 15:53:55 +0700

    update for netter routes

--- routes/web.php
+++ routes/web.php
@@ -20,11 +20,3 @@ Route::get('/', function () {
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Auth::routes();
