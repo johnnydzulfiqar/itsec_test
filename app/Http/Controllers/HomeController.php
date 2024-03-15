720693ea6b342d1df0cb6c7acda0c2c8f586797f
M Naufa Dzulfiqar <muhammadnaufa007@gmail.com>
2024-03-15 15:59:44 +0700

    add feature for index admin to see detail information every users

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class HomeController extends Controller
 {
@@ -23,6 +25,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $data['data'] = User::all();
+        return view('home', $data);
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,37 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container-fluid">
-        <h1 class="text-black-50">You are logged in!</h1>
+<div class="col-md-12">
+    <div class="card">
+        <div class="card-header">
+            <h4>Tabel User</h4>
+        </div>
+        <div class="card-body">
+      </div>
+            <table class="table table-bordered table-hover" id="table_id">
+                <thead>
+                    <tr>
+                      <th>No</th>
+                      <th>Nama</th>
+                      <th>Email</th>
+                      <th>Created_at</th>
+                    </tr>
+                </thead>
+                <tbody>
+                  @foreach ($data as $item)
+                  <tr>
+                    <td>{{ $loop->iteration }}</td>
+                                  <td>{{ $item->name }}</td>
+                                  <td>{{ $item->email }}</td>
+                                  <td>{{ $item->created_at }}</td>
+                  </tr>
+                  @endforeach
+                </tbody>
+             
+            </table>
+
+        </div>
+
     </div>
+</div>
 @endsection
