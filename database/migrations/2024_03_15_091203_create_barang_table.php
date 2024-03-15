8dcbe5204fda5902221cb40eda553442ad1760d7
M Naufa Dzulfiqar <muhammadnaufa007@gmail.com>
2024-03-15 16:19:29 +0700

    adding routes for home & barang index in sidebar

--- database/migrations/2024_03_15_091203_create_barang_table.php
+++ database/migrations/2024_03_15_091203_create_barang_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('barang', function (Blueprint $table) {
             $table->id();
+            $table->string('nama_barang', 150);
+            $table->integer('stok');
+            $table->integer('harga_barang');
             $table->timestamps();
         });
     }
--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -5,15 +5,23 @@
              class="brand-image img-circle elevation-3">
         <span class="brand-text font-weight-light">{{ config('app.name') }}</span>
     </a>
-    <a href="{{ route('barang') }}" class="brand-link">
 
-    </a>
 
     <div class="sidebar">
         <nav class="mt-2">
             <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-                @include('layouts.menu')
+               
+                <li>
+                    <a href="{{ route('home') }}" class="brand-link">
+                    Home
+                    </a>
+                </li>
+                    <a href="{{ route('barang.index') }}" class="brand-link">
+                        Barang
+                    </a>
+                <li>
             </ul>
+            
         </nav>
     </div>
 
